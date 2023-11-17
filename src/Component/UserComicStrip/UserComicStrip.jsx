330ef355d1a1145b0cc661971a846f295da686aa
unknown <llakshya63@gmail.com>
2023-11-18 02:51:56 +0530

    google font is added

--- src/Component/UserComicStrip/UserComicStrip.jsx
+++ src/Component/UserComicStrip/UserComicStrip.jsx
@@ -10,7 +10,7 @@ function UserComicStrip() {
   // if (ImageStore.length === 0) return;
   return (
     <div className="py-6 text-white flex justify-center items-center">
-      <div className="w-[80%] flex flex-col gap-6 justify-center items-center">
+      <div className="w-[80%] flex flex-col gap-10 justify-center items-center">
         <h1 className="text-center font-bold max-[700px]:text-[20px] text-2xl">Your Comic Strip</h1>
         <div className="grid max-[1200px]:grid-cols-2 max-[450px]:grid-cols-1 grid-cols-3 gap-10">
           <div>
